bc394a8f87e9003dec7c525948d9b1a2b80b5add
Jareer <jforeverything2007@gmail.com>
2024-02-10 21:24:44 +0100

    Update bucket.cpp

--- libs/bucket.cpp
+++ libs/bucket.cpp
@@ -1,11 +1,14 @@
 #ifndef DB_BUCKET
 #define DB_BUCKET
 
+#include <algorithm>
 #include <cstdint>
 #include <stdint.h>
 #include <unordered_map>
 #include "./datatypes.cpp"
 #include <string>
+#include <vector>
+#include <iostream>
 
 struct BucketSchemaField {
   uint64_t max_len;
@@ -17,7 +20,7 @@ public:
   std::unordered_map<std::string, BucketSchemaField> schema;
   bool strict_type;
   
-  // HashMap<RowName, Row<Id, Data>
+  // HashMap<Id, Row<RowName, Data>
   std::unordered_map<uint64_t, std::unordered_map<std::string, BucketDataTypes>> rows;
 
   Bucket() {
@@ -27,6 +30,16 @@ public:
   void AppendType(std::string name, BucketSchemaField sf) {
     schema[name] = sf;
   }
+
+  std::vector<std::unordered_map<std::string, BucketDataTypes>> Search(std::string key, Data ) {
+    std::vector<std::unordered_map<std::string, BucketDataTypes>> results;
+
+    std::for_each(rows.begin(), rows.end(), [](auto row){
+      std::cout << row.second.find("") << "\n";
+    });
+
+    return results;
+  };
 };
 
 #endif // !DB_BUCKET
